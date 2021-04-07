813858e303d67236058242754e6d705dc971c9fb
chris_mlvz <chris.mlvz@gmail.com>
2021-04-07 18:37:34 -0500

    raise and excepts

--- debugging.py
+++ debugging.py
@@ -1,16 +1,24 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-        breakpoint()
         if num % i == 0:
             divisors.append(i)
     return divisors
 
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input("Ingresa un número: "))
+        if num <= 0:
+            raise Exception("El número debe ser positivo")
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un número")
+    except Exception as e:
+        print(e)
+    
+
 
 if __name__ == '__main__':
     run()
