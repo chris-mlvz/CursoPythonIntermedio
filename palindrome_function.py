dfd7b1db408be62bb154e56d4acf5fa40914e06a
chris_mlvz <chris.mlvz@gmail.com>
2021-04-07 18:21:24 -0500

    try, except and rise

---
+++ palindrome_function.py
@@ -0,0 +1,16 @@
+from typing import Final
+
+
+def palindrome(string):
+    try:
+        if len(string) == 0:
+            raise ValueError("No se puede ingresar una cadena vacía")
+        return string == string[::-1]
+    except ValueError as ve:
+        print(ve)
+        return False
+
+try: 
+    print(palindrome(""))
+except TypeError:
+    print("Solo se pueden ingresar strings")
